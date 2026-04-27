45cbed19a5921d1f4dae3b4a82979561c1ca7030
m8n-matandessaur <matandessaur@gmail.com>
2026-04-27 16:12:46 -0400

    Fix apps provider routing and failover

---
+++ scripts/Invoke-AppsDo.ps1
@@ -0,0 +1,22 @@
+param(
+  [Parameter(Mandatory = $true)][string]$App,
+  [Parameter(Mandatory = $true)][string]$Goal,
+  [int]$WaitMs = 600000,
+  [string]$Provider,
+  [string]$Model
+)
+
+$body = @{
+  app = $App
+  goal = $Goal
+  waitMs = $WaitMs
+}
+
+if ($Provider) { $body.provider = $Provider }
+if ($Model) { $body.model = $Model }
+if ($env:SYMPHONEE_TERM_ID) { $body.termId = $env:SYMPHONEE_TERM_ID }
+
+Invoke-RestMethod -Method Post `
+  -Uri 'http://127.0.0.1:3800/api/apps/do' `
+  -ContentType 'application/json' `
+  -Body ($body | ConvertTo-Json -Depth 10)
